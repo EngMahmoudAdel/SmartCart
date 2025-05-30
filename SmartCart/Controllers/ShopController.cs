b442393c6a4dc7cfca7aac7be12cd457257a5ba0
MAHMOUD <mahmoud192548@fci.bu.edu.eg>
2025-05-30 04:36:30 +0300

    products from db

--- SmartCart/Controllers/ShopController.cs
+++ SmartCart/Controllers/ShopController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmartCart.Data.Interfaces;
 
 namespace SmartCart.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index()
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShopController(IUnitOfWork unitOfWork)
         {
-            return View();
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _unitOfWork.Products.FindAllAsync();
+            return View(products);
         }
     }
 }
